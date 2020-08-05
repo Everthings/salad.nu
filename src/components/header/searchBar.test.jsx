935ff1aab691f0ab8ede1923e70d4582c7a9c826
Andy Xu <andyxu12@gmail.com>
2020-08-04 21:02:26 -0400

    Fixed searchBar tests

--- src/components/header/searchBar.test.jsx
+++ src/components/header/searchBar.test.jsx
@@ -1,20 +1,46 @@
 import React from "react";
 import renderer from "react-test-renderer";
 import { render, cleanup } from "@testing-library/react";
+import { Provider } from "react-redux";
+import configureStore from "redux-mock-store";
 import SearchBar from "./searchBar";
+import Theme from "../themes/theme";
+
+const initialState = {
+  entities: {
+    interactions: { searchStr: "", currentTheme: { theme: "green" } },
+    term: { name: "2019 Fall" },
+    schedule: { list: [] },
+  },
+};
+const mockStore = configureStore([]);
 
 afterEach(cleanup);
 
 describe("SearchBar", () => {
   it("renders correctly", () => {
-    const tree = renderer.create(<SearchBar />).toJSON();
+    const store = mockStore(initialState);
+    const tree = renderer
+      .create(
+        <Provider store={store}>
+          <Theme>
+            <SearchBar />
+          </Theme>
+        </Provider>
+      )
+      .toJSON();
     expect(tree).toMatchSnapshot();
   });
 
   it("should display search value", () => {
+    const store = mockStore(initialState);
     const handleChange = () => 0;
     const { getByTestId } = render(
-      <SearchBar search="some search" handleChange={handleChange} />
+      <Provider store={store}>
+        <Theme>
+          <SearchBar search="some search" handleChange={handleChange} />
+        </Theme>
+      </Provider>
     );
 
     const input = getByTestId("search-bar");
@@ -23,10 +49,19 @@ describe("SearchBar", () => {
   });
 
   it("should display placeholder value with term", () => {
+    const store = mockStore(initialState);
     const handleChange = () => 0;
     const term = { name: "some term" };
     const { getByTestId } = render(
-      <SearchBar search="some search" term={term} handleChange={handleChange} />
+      <Provider store={store}>
+        <Theme>
+          <SearchBar
+            search="some search"
+            term={term}
+            handleChange={handleChange}
+          />
+        </Theme>
+      </Provider>
     );
 
     const input = getByTestId("search-bar");
