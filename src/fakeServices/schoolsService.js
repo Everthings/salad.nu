a7df73747f46661e97aaac63c514628537543dd4
Andy Xu <andyxu12@gmail.com>
2021-05-16 19:17:53 -0500

    Updated summer 2021 data

--- src/fakeServices/schoolsService.js
+++ src/fakeServices/schoolsService.js
@@ -1,17 +1,11 @@
 const schools = [
   { symbol: "JOUR", name: "Medill School of Journalism" },
-  { symbol: "KGSM", name: "J L Kellogg  School Management" },
   { symbol: "LAW", name: "Law School" },
-  {
-    symbol: "MEAS",
-    name: "McCormick School of Engineering and Applied Science",
-  },
-  { symbol: "MUSIC", name: "Bienen School of Music" },
+  { symbol: "MEAS", name: "McCmick Schl of Engg & App Sci" },
   { symbol: "SESP", name: "School of Educ & Social Policy" },
   { symbol: "SPCH", name: "School of Communication" },
-  { symbol: "TGS", name: "The Graduate School" },
   { symbol: "UC", name: "School of Professional Studies" },
-  { symbol: "WCAS", name: "Weinberg College of Arts and Sciences" },
+  { symbol: "WCAS", name: "Weinberg College of Arts & Sci" },
 ];
 
 export function getSchools() {
